8b6ca7e51806fcb65de57b54c35846a79fbacbf4
André Sugai <guitarfreaks@gmail.com>
2023-02-17 01:46:55 -0300

    feat: implementando o highscore

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ console.log(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1; // incluindo o +1 garantimos que o resultado seja até o número 20, sem ele o resultado só iria até 19.9999
 let score = 20;
 
+let highScore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -37,6 +39,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
 
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // Quando o jogador erra com um número maior
   } else if (guess > secretNumber) {
