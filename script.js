b6c505a029d81316d89dd295c8295af325dba6ba
André Sugai <guitarfreaks@gmail.com>
2023-02-17 01:34:25 -0300

    feat: implementando a função de reset

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ console.log(document.querySelector('.guess').value);
 
 // Handling Click Events
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1; // incluindo o +1 garantimos que o resultado seja até o número 20, sem ele o resultado só iria até 19.9999
+let secretNumber = Math.trunc(Math.random() * 20) + 1; // incluindo o +1 garantimos que o resultado seja até o número 20, sem ele o resultado só iria até 19.9999
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -64,3 +64,35 @@ document.querySelector('.check').addEventListener('click', function () {
 
 // Implementing the Game Logic
 // Manipulating CSS Styles
+
+// Coding Challenge #1
+
+// Implement a game reset functionality, so that the player can make a new guess!
+
+// Your tasks:
+// 1. Select the element with the 'again' class and attach a click event handler
+
+document.querySelector('.again').addEventListener('click', function () {
+  let score = 20;
+
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('.guess').value = ' ';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.number').style.width = '15rem';
+});
+
+// 2. In the handler function, restore initial values of the 'score' and 'secretNumber' variables
+
+// 3. Restore the initial conditions of the message, number, score and guess input fields
+
+// 4. Also restore the original background color (#222) and number width (15rem)
+// GOOD LUCK �
