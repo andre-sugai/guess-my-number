f8e864e044d947e260628382cbc74969fa0064e1
André Sugai <guitarfreaks@gmail.com>
2023-02-16 22:28:00 -0300

    feat: adicionando eventlistener no botão e captura do valor do input

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
+
+// Selecting and Manipulating Elements
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+*/
+
+// Handling Click Events
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🤨 No number!';
+  }
+});
